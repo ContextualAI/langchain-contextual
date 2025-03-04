ec74b69dec33c3a6a016a1ac13e1e163d8c2555e
Jonathan Feng <10607618+jonathanfeng@users.noreply.github.com>
2025-03-03 23:54:27 -0800

    add new parameters

--- langchain-contextual/langchain_contextual/chat_models.py
+++ langchain-contextual/langchain_contextual/chat_models.py
@@ -42,6 +42,15 @@ class ChatContextual(BaseChatModel):
     base_url: Optional[str] = Field(default=None)
     """Base URL of Contextual AI Application."""
 
+    temperature: Optional[float] = Field(default=0)
+    """The sampling temperature, which affects the randomness in the response. Note that higher temperature values can reduce groundedness. Defaults to `0`."""
+
+    top_p: Optional[float] = Field(default=0.9)
+    """A parameter for nucleus sampling, an alternative to temperature which also affects the randomness of the response. Note that higher top_p values can reduce groundedness. Defaults to `0.9`."""
+
+    max_new_tokens: Optional[int] = Field(default=1024)
+    """The maximum number of tokens that the model can generate in the response. Defaults to 1024. Minimum is 1. Maximum is 2048"""
+
     model_name: str = Field(default="v1", alias="model")
     """The name of the model. Defaults to `v1`."""
 
@@ -131,6 +140,9 @@ class ChatContextual(BaseChatModel):
             model=self.model_name,
             system_prompt=system_prompt,
             avoid_commentary=avoid_commentary,
+            max_new_tokens=self.max_new_tokens,
+            temperature=self.temperature,
+            top_p=self.top_p,
         )
         message = AIMessage(
             content=raw_message.to_dict().get("response"),
